8386237f04516a7878258995d0d79a8d30af7f90
Wahiba BOUZYAN <123891320+Wahiba275@users.noreply.github.com>
2023-12-30 19:18:32 +0100

    Delete ressource-service directory

--- ressource-service/src/main/java/ma/enset/ressourceservice/enums/TypeResource.java
+++
@@ -1,5 +0,0 @@
-package ma.enset.ressourceservice.enums;
-
-public enum TypeResource {
-    MATERIEL_INF0, MATERIEL_AUDIO_VUSUEL
-}
--- ressource-service/src/main/java/ma/enset/ressourceservice/repository/ResourceRepository.java
+++
@@ -1,8 +0,0 @@
-package ma.enset.ressourceservice.repository;
-
-
-import ma.enset.ressourceservice.entities.Ressource;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ResourceRepository extends JpaRepository<Ressource, Long> {
-}
